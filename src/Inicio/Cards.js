8bdf8ad028120a2b7425769790b3cba429758f66
abombero <agustin.wvonb@gmail.com>
2023-07-12 10:40:00 -0300

    local storage

--- src/Inicio/Cards.js
+++ src/Inicio/Cards.js
@@ -1,13 +1,30 @@
 import { Items } from "./Items";
+import { useState } from "react";
 
 export function Cards({ colec, figures, newFigures }) {
+  const [searchInput, setSearchInput] = useState("");
   console.log(newFigures);
   newFigures.map((f) =>
     colec.map((c) => (c.id === f.collection ? (f.logo = c.logo) : ""))
   );
 
+  const [inputText, setInputText] = useState("");
+
+  let inputHandler = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
     <div className="collections">
+      <input
+        type="text"
+        placeholder="Buscar figuras"
+        onChange={inputHandler}
+        value={searchInput}
+      />
+
+      <button>BUSCAR</button>
       {colec.map((col) => (
         <section key={col.collection} id={col.id}>
           <img id="banner" src={col.banner} alt={col.banner} />
